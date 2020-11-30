b8c7e489f2229366d5eee0d07c79132f6702591b
Logharaa <gaelmeheust78@gmail.com>
2020-11-30 09:51:39 +0100

    TP Service Workers V3

--- service_workers/sw.js
+++ service_workers/sw.js
@@ -1,23 +1,30 @@
-this.addEventListener('install', function (event) {
+this.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open('v1').then(function (cache) {
+        caches.open("v1").then(function (cache) {
             return cache.addAll([
-                'index.html',
-                '../style.css',
+                "index.html",
+                "../style.css",
             ]);
         })
     );
 });
 
-this.addEventListener('fetch', function (event) {
-    let response;
-    event.respondWith(caches.match(event.request).catch(function () {
-        return fetch(event.request);
-    }).then(function (r) {
-        response = r;
-        caches.open('v1').then(function (cache) {
-            cache.put(event.request, response);
-        });
-        return response.clone();
+this.addEventListener("fetch", function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+        if (response !== undefined) {
+            return response;
+        }
+        else {
+            return fetch(event.request).then(function(response) {
+                let responseClone = response.clone();
+
+                caches.open("v1").then(function(cache) {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            }).catch(function() {
+                return "Et non !"
+            });
+        }
     }));
 });
