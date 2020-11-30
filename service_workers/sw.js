fdc85e3de035a1df05b0e73b41d76d1903655ae2
Logharaa <gaelmeheust78@gmail.com>
2020-11-30 09:22:37 +0100

    TP Service Workers V2

--- service_workers/sw.js
+++ service_workers/sw.js
@@ -1,10 +1,23 @@
-this.addEventListener('install', function(event) {
+this.addEventListener('install', function (event) {
     event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          'index.html',
-          '../style.css',
-        ]);
-      })
+        caches.open('v1').then(function (cache) {
+            return cache.addAll([
+                'index.html',
+                '../style.css',
+            ]);
+        })
     );
-  });
+});
+
+this.addEventListener('fetch', function (event) {
+    let response;
+    event.respondWith(caches.match(event.request).catch(function () {
+        return fetch(event.request);
+    }).then(function (r) {
+        response = r;
+        caches.open('v1').then(function (cache) {
+            cache.put(event.request, response);
+        });
+        return response.clone();
+    }));
+});
